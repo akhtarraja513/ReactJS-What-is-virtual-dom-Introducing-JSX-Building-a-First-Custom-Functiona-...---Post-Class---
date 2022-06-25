78bd6ecd18219ac0766f9884691fb428d86007e3
akhtarraja513 <akhtarraja513@gmail.com>
2022-06-25 17:56:42 +0530

    Updated project

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,17 @@
 // write code for Welcome component here
+import React, { Component, useState } from "react";
+import '../styles/App.css';
+
+const Welcome = ({name}) => {
+   let hey = `Hey ! ${name}`;
+   let greetMessage = `Welcome to Newton School.`;
+  return (
+    <div className="main">
+      <h1>{hey}</h1>
+      <h2>{greetMessage}</h2>
+    </div>
+  )
+}
+
+
+export default Welcome;
