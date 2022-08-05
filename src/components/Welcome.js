ee716ff11bf94332782ff6eae6ae2efe8e15533a
akhtarraja513 <akhtarraja513@gmail.com>
2022-08-05 17:25:51 +0530

    Again Done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,11 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const Welcome = (props) => {
-   let hey = `Hey ! ${props.name}`;
+  //  let hey = `Hey ! ${props.name}`;
    let greetMessage = `Welcome to Newton School.`;
   return (
     <div className="main">
-      <h1>{hey}</h1>
+      <div>Hey ! <h1>{props.name}</h1></div>
       <h2>{greetMessage}</h2>
     </div>
   )
