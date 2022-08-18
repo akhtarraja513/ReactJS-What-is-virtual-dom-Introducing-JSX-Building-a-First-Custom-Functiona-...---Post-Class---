fe419657f44f36a4cc0b16db7f9fdb1808affe56
akhtarraja513 <akhtarraja513@gmail.com>
2022-08-18 11:03:51 +0530

    jhfgbcd

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,11 +4,11 @@ import '../styles/App.css';
 
 const Welcome = (props) => {
   //  let hey = `Hey ! ${props.name}`;
-   let greetMessage = `Welcome to Newton School.`;
+  //  let greetMessage = `Welcome to Newton School.`;
   return (
     <div className="main">
       <h1>Hey ! {props.name}</h1>
-      <h2>{greetMessage}</h2>
+      <h2>Welcome to Newton School.</h2>
     </div>
   )
 }
